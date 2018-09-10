067bcc4f21eef68f6b21f7b5cda15c1f8e738bd9
3334637220 <04637@163.com>
2018-09-10 20:55:37 +0800

    update

--- src/base1/Search.java
+++ src/base1/Search.java
@@ -1,45 +1,103 @@
 package base1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
- * 输出na中不存在于ma中的元素
- * 例: ma [1,3,5,9,12] na[2,3,5,7,10]
+ * 输出ma中不存在于na中的元素
+ * na为有序数组, 长度N, ma无序, 长度M
+ * 例: na [1,3,5,9,12] ma[7,2,5,3,10]
  * 输出: 2,7
- *
  */
 public class Search {
 
     public static void main(String[] args) {
-        final int m = 10;
-        final int n = 5;
-        int[] ma = new int[m];
-        int[] na = new int[n];
+        final int n = 10;
+        final int m = 5;
+        int[] na = new int[m];
+        int[] ma = new int[n];
         Random rand = new Random();
         for (int i = 0; i < m; i++) {
-            ma[i] = rand.nextInt(100);
-        }
-        for (int i = 0; i < n; i++) {
             na[i] = rand.nextInt(100);
         }
-        Arrays.sort(ma);
+        for (int i = 0; i < n; i++) {
+            ma[i] = rand.nextInt(100);
+        }
         Arrays.sort(na);
+        List<Integer> result = binarySearch(na, ma);
+        System.out.println(result);
+    }
 
-        System.out.println("ma: " + Arrays.toString(ma));
+
+    /**
+     * 二分
+     * 时间复杂度: O(M*logN)
+     * @param na 有序数组
+     * @param ma 无序数组
+     */
+    public static List<Integer> binarySearch(int[] na, int[] ma) {
+        List<Integer> result = new ArrayList<>();
         System.out.println("na: " + Arrays.toString(na));
-        for (int a = 0, b = 0; a < m && b < n; ) {
-            if (ma[a] < na[b]) {
+        System.out.println("ma: " + Arrays.toString(ma));
+        for (int i = 0; i < ma.length; i++) {
+            int left = 0;
+            int right = na.length - 1;
+            int mid = (right - left) >>> 2;
+            boolean exists = false;
+            for (; left <= right; ) {
+                if (ma[i] < na[mid]) {
+                    right = mid - 1;
+                    mid = (right + left) >>> 2;
+                } else if (ma[i] > na[mid]) {
+                    left = mid + 1;
+                    mid = (right + left) >>> 2;
+                } else {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                result.add(ma[i]);
+            }
+        }
+
+        return result;
+
+    }
+
+    /**
+     * 外排     排序复杂度↓
+     * 时间复杂度: O(M*logM)+O(N+M)
+     * @param na 有序数组
+     * @param ma 需要对其排序
+     * @return result
+     */
+    public static List<Integer> search(int[] na, int[] ma) {
+        int m = na.length;
+        int n = ma.length;
+        Arrays.sort(ma);
+        System.out.println("na: " + Arrays.toString(na));
+        System.out.println("ma: " + Arrays.toString(ma));
+        int a = 0;
+        int b = 0;
+        List<Integer> result = new ArrayList<>();
+        for (; a < m && b < n; ) {
+            if (na[a] < ma[b]) {
                 a++;
-            } else if (ma[a] > na[b]) {
-                System.out.print(na[b]+", ");
+            } else if (na[a] > ma[b]) {
+                result.add(ma[b]);
                 b++;
             } else {
                 b++;
             }
         }
+        for (int i = b; i < n; i++) {
+            result.add(ma[b]);
+        }
+
+        return result;
     }
-
-
 
 }
---
+++ src/base1/Sort.java
@@ -0,0 +1,70 @@
+package base1;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Sort {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int len = 15;
+        int[] a = new int[len];
+        for (int i = 0; i < len; i++) {
+            a[i] = rand.nextInt(100);
+        }
+        System.out.println(Arrays.toString(a));
+        insertSort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    /**
+     * 插入排序
+     * 时间复杂度: O(N^2)
+     */
+    public static void insertSort(int[] a){
+        for(int i=1;i<a.length;i++){
+            for(int j=i-1;j>=0&&a[j]>a[j+1];j--){
+                swap(a, j, j+1);
+            }
+        }
+    }
+
+
+    /**
+     * 冒泡排序
+     * 时间复杂度: O(N^2)
+     */
+    public static void bubbleSort(int[] a) {
+
+        for (int end = a.length - 1; end > 0; end--) {
+            for (int i = 0; i < end; i++) {
+                if (a[i] > a[i + 1]) {
+                    swap(a, i, i + 1);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 选择排序
+     * 时间复杂度: O(N^2)
+     */
+    public static void selectSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < a.length; j++) {
+                minIndex = a[j] < a[minIndex] ? j : minIndex;
+            }
+            swap(a, i, minIndex);
+        }
+    }
+
+
+    private static void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+
+}
